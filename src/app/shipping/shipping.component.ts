58de813827eb65ac5c944d86292b817fc518165d
dongyongzhi <dongyongzhi@foxmail.com>
2020-12-17 16:11:03 +0800

    3 files modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-
+import { FormBuilder } from "@angular/forms"
 
 @Component({
   selector: 'app-shipping',
@@ -9,11 +9,18 @@ import { CartService } from '../cart.service';
 })
 export class ShippingComponent implements OnInit {
    shippingCosts;
+   checkoutForm;
 
   constructor(
-     private cartservice : CartService
+     private cartservice : CartService,
+     private formBuilder : FormBuilder
 
-  ) { }
+  ) { 
+      this.checkoutForm = this.formBuilder.group({
+        name: '',
+        address: ''
+    });
+  }
 
   ngOnInit() {
 
@@ -25,4 +32,9 @@ export class ShippingComponent implements OnInit {
     return this.cartservice.getShopingPrice();
   }
 
+  onsubmit(customData){
+     this.cartservice.clearItems();
+     this.checkoutForm.reset();
+     console.warn("your order has been  submitted",customData);
+  }
 }
